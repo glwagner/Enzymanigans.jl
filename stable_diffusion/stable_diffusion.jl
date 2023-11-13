902fa70d6ab450be45453a44894018ea85def036
jlk9 <josek97@utexas.edu>
2023-11-13 13:24:23 -0600

    Reducing the reduced example because I found another bug

--- stable_diffusion/stable_diffusion.jl
+++ stable_diffusion/stable_diffusion.jl
@@ -3,11 +3,24 @@ using Oceananigans.TurbulenceClosures: with_tracers
 using Oceananigans.Models.HydrostaticFreeSurfaceModels: tracernames
 using Enzyme
 
+using Oceananigans.Fields
+using Oceananigans.Fields: default_indices
+using Oceananigans.BoundaryConditions
+using Oceananigans.BoundaryConditions: FieldBoundaryConditions, regularize_field_boundary_conditions
+using Oceananigans: instantiated_location
+
+using Oceananigans.BoundaryConditions: extract_bottom_bc
+
+using OffsetArrays: OffsetArray
+
+include("../../FlattenedTuples/FlattenedTuples.jl")
+import .FlattenedTuples: reduced_flattened_unique_values
+
 Enzyme.API.runtimeActivity!(true)
 #Enzyme.API.printall!(true)
 Enzyme.EnzymeRules.inactive_type(::Type{<:Oceananigans.Grids.AbstractGrid}) = true
 Enzyme.EnzymeRules.inactive_noinl(::typeof(Core._compute_sparams), args...) = nothing
-
+#=
 const maximum_diffusivity = 100
 
 grid = RectilinearGrid(size=128, z=(-0.5, 0.5), topology=(Flat, Flat, Bounded))
@@ -32,64 +45,88 @@ function set_diffusivity!(model, diffusivity)
     return nothing
 end
 
-function set_initial_condition!(model, amplitude)
-    # Set initial condition
-    width = 0.1
-    cᵢ(z) = amplitude * exp(-z^2 / (2width^2))
-    set!(model, c=cᵢ)
+function reduced_fill_halo_regions!(maybe_nested_tuple::Union{NamedTuple, Tuple})
+
+    #ordinary_fields = maybe_nested_tuple
+    # THIS function call (flattened_unique_values) leads to the seg fault, if we just set ordinary_fields = maybe_nested_tuple
+    # then no errors
+    ordinary_fields = reduced_flattened_unique_values(maybe_nested_tuple)
+
+    @show ordinary_fields
+    @show typeof(ordinary_fields)
+    
+    fill_halos! = [fill_west_and_east_halo!, fill_west_and_east_halo!]
+    bcs  = reduced_permute_boundary_conditions(map(boundary_conditions, ordinary_fields))
+    number_of_tasks   = length(fill_halos!)
+
+    # Fill halo in the three permuted directions (1, 2, and 3), making sure dependencies are fulfilled
+    for task = 1:number_of_tasks
+        fill_halos![task](map(data, ordinary_fields), bcs[task])
+    end
+    
     return nothing
 end
 
-function stable_diffusion!(model, amplitude, diffusivity)
-    set_diffusivity!(model, diffusivity)
-    set_initial_condition!(model, amplitude)
-    
-    # Do time-stepping
-    Nx, Ny, Nz = size(model.grid)
-    κ_max = maximum_diffusivity
-    Δz = 2π / Nz
-    Δt = 1e-1 * Δz^2 / κ_max
-
-    model.clock.time = 0
-    model.clock.iteration = 0
-
-    for n = 1:10
-        time_step!(model, Δt; euler=true)
-    end
-
-    # Compute scalar metric
-    c = model.tracers.c
-
-    # Hard way
-    # c² = c^2
-    # sum_c² = sum(c²)
-
-    # Another way to compute it
-    c² = Array(interior(c).^2)
-    sum_c² = sum(c²)
-
-    return sum_c²::Float64
+function boundary_conditions(f::Field)
+    return f.boundary_conditions
 end
 
-@show(model)
+function reduced_permute_boundary_conditions(boundary_conditions)
+    #=
+    @show boundary_conditions
+    @show typeof(boundary_conditions)
+    @show extract_bottom_bc(boundary_conditions)
+    @show typeof(extract_bottom_bc(boundary_conditions))
+    =#
+    sides       = [:west_and_east, :bottom_and_top]
+    boundary_conditions = Tuple(extract_bc(boundary_conditions, Val(side)) for side in sides)
 
-# Compute derivative by hand
-κ₁, κ₂ = 0.9, 1.1
-c²₁ = stable_diffusion!(model, 1, κ₁)
-c²₂ = stable_diffusion!(model, 1, κ₂)
-dc²_dκ = (c²₂ - c²₁) / (κ₂ - κ₁)
+    return boundary_conditions
+end
 
+@inline extract_bc(bc, ::Val{:west_and_east})   = ((nothing,))
+@inline extract_bc(bc, ::Val{:bottom_and_top})  = (extract_bottom_bc(bc))
+
+#@inline extract_bc(bc, ::Val{:bottom_and_top})  = ((ReducedBoundaryCondition(Flux(), nothing),), (ReducedBoundaryCondition(Flux(), nothing),))
+fill_west_and_east_halo!(c, west_bc) = 0
+
+#=
+abstract type AbstractBoundaryConditionClassification end
+struct Flux <: AbstractBoundaryConditionClassification end
+struct ReducedBoundaryCondition{C<:AbstractBoundaryConditionClassification, T}
+    classification :: C
+    condition :: T
+end
+@show Oceananigans.prognostic_fields(model)
+@show typeof(Oceananigans.prognostic_fields(model))
+
+@show model.closure
+@show typeof(model.closure)
+=#
 # Now for real
-amplitude = 1.0
-κ = 1.0
 dmodel = deepcopy(model)
 set_diffusivity!(dmodel, 0)
 
-@show(model)
-@show(c²₁)
-@show(c²₂)
+#reduced_fill_halo_regions!(Oceananigans.prognostic_fields(model))
 
-autodiff(Reverse, set_initial_condition!, Duplicated(model, dmodel), Active(amplitude))
-#autodiff(Reverse, set_diffusivity!, Duplicated(model, dmodel), Active(κ))
-#autodiff(Reverse, stable_diffusion!, Duplicated(model, dmodel), Const(amplitude), Active(κ))
+autodiff(Reverse, reduced_fill_halo_regions!, Duplicated(Oceananigans.prognostic_fields(model), Oceananigans.prognostic_fields(dmodel)))
 
+=#
+
+big_tuple = ((1,2), (2,(4,5)), 3, 2, 1)
+
+d_big_tuple = ((0,0), (0,(0,0)), 0, 0, 0)
+
+flattened_big_tuple = flattened_unique_values(big_tuple)
+reduced_flattened_big_tuple = reduced_flattened_unique_values(big_tuple)
+
+@show flattened_big_tuple
+@show reduced_flattened_big_tuple
+
+function has_flattened_tuple!(maybe_nested_tuple::Union{NamedTuple, Tuple})
+
+    flattened_tuple = flattened_unique_values(maybe_nested_tuple)
+    return nothing
+end
+
+autodiff(Reverse, has_flattened_tuple!, Duplicated(big_tuple, d_big_tuple))
