3d956097dce7df366e626cf6cfb1f9c3f5ba3729
jlk9 <josek97@utexas.edu>
2024-05-28 20:19:47 -0500

    Edits

--- stable_diffusion/enzyme_oceananigans_init.jl
+++ stable_diffusion/enzyme_oceananigans_init.jl
@@ -11,9 +11,15 @@ EnzymeRules.inactive_type(::Type{<:Oceananigans.Grids.AbstractGrid}) = true
 EnzymeRules.inactive_type(::Type{<:Oceananigans.Clock}) = true
 
 Enzyme.API.looseTypeAnalysis!(true)
+Enzyme.API.maxtypeoffset!(3000)
 
-function set_initial_condition!(model, cᵢ)
+function set_initial_condition!(model)
+    amplitude = Ref(1.0)
+
+    # This has a "width" of 0.1
+    cᵢ(x, y, z) = amplitude[]
     set!(model, c=cᵢ)
+
     return nothing
 end
 
@@ -27,16 +33,13 @@ topology = (Periodic, Periodic, Bounded)
 grid = RectilinearGrid(size=(Nx, Ny, Nz); x, y, z, topology)
 
 model = HydrostaticFreeSurfaceModel(; grid,
-                                        tracers = :c,
-                                        buoyancy = nothing)
-
-
-amplitude   = Ref(amplitude)
-cᵢ(x, y, z) = amplitude[]
+                                      tracers = :c,
+                                      buoyancy = nothing)
 
 # Test differentiation of the high-level set interface
+set_initial_condition!(model)
+
 dmodel = Enzyme.make_zero(model)
 autodiff(Enzyme.Reverse,
-                set_initial_condition!,
-                Duplicated(model, dmodel),
-                Const(cᵢ))
+         set_initial_condition!,
+         Duplicated(model, dmodel))
