8d66f6824c74af77ed30fea8b0d558dfe98bc957
jlk9 <josek97@utexas.edu>
2024-07-02 13:57:25 -0500

    Reducing for flux BC error

--- stable_diffusion/stable_diffusion.jl
+++ stable_diffusion/stable_diffusion.jl
@@ -12,7 +12,7 @@ Enzyme.API.runtimeActivity!(true)
 #Enzyme.API.printall!(true)
 #Enzyme.API.printactivity!(true)
 Enzyme.API.looseTypeAnalysis!(true)
-Enzyme.EnzymeRules.inactive_type(::Type{<:Oceananigans.Grids.AbstractGrid}) = true
+#Enzyme.EnzymeRules.inactive_type(::Type{<:Oceananigans.Grids.AbstractGrid}) = true
 Enzyme.EnzymeRules.inactive_type(::Type{<:Oceananigans.Clock}) = true
 Enzyme.EnzymeRules.inactive_noinl(::typeof(Core._compute_sparams), args...) = nothing
 
@@ -66,30 +66,8 @@ model = HydrostaticFreeSurfaceModel(; grid,
                                     tracers = :c,
                                     buoyancy = nothing,
                                     velocities = PrescribedVelocityFields(; u, v),
-                                    #boundary_conditions = (; c=c_bcs),
+                                    boundary_conditions = (; c=c_bcs),
                                     closure = diffusion)
-
-#=
-L = 0.5
-cᵢ(x, y, z) = exp(-(x^2 + y^2) / 2L^2)
-set!(model, c=cᵢ)
-Δt = U * 0.2π / Nx
-simulation = Simulation(model; Δt, stop_time = 1)
-progress(sim) = @info string("Iter: ", iteration(sim), ", time: ", time(sim))
-simulation.callbacks[:progress] = Callback(progress, IterationInterval(10))
-run!(simulation)
-
-c = model.tracers.c
-using GLMakie
-fig = Figure()
-axu = Axis(fig[1, 1])
-axv = Axis(fig[1, 2])
-axc = Axis(fig[1, 3])
-heatmap!(axu, interior(u, :, :, 1))
-heatmap!(axv, interior(v, :, :, 1))
-heatmap!(axc, interior(c, :, :, 1))
-display(fig)
-=#
 
 """
     set_diffusivity!(model, diffusivity)
@@ -109,67 +87,24 @@ function set_initial_condition!(model, amplitude)
     amplitude = Ref(amplitude)
 
     # This has a "width" of 0.1
-    cᵢ(x, y, z) = amplitude[] * exp(-z^2 / 0.02 - (x^2 + y^2) / 0.05)
+    cᵢ(x, y, z) = amplitude[]
     set!(model, c=cᵢ)
 
     return nothing
 end
 
-function stable_diffusion!(model, amplitude, diffusivity)
-    set_diffusivity!(model, diffusivity)
-    set_initial_condition!(model, amplitude)
-    
-    # Do time-stepping
-    Nx, Ny, Nz = size(model.grid)
-    κ_max = maximum_diffusivity
-    Δz = 2π / Nz
-    Δt = 1e-1 * Δz^2 / κ_max
-
-    model.clock.time = 0
-    model.clock.iteration = 0
-
-    for n = 1:10
-        time_step!(model, Δt; euler=true)
-    end
-
-    # Compute scalar metric
-    c = model.tracers.c
-
-    # Hard way
-    # c² = c^2
-    # sum_c² = sum(c²)
-
-    # Another way to compute it
-    sum_c² = 0.0
-    for i = 1:Nx, j = 1:Ny, k = 1:Nz
-        sum_c² += c[i, j, k]^2
-    end
-
-    return sum_c²::Float64
-end
-
-# Compute derivative by hand
-κ₁, κ₂ = 0.9, 1.1
-c²₁ = stable_diffusion!(model, 1, κ₁)
-c²₂ = stable_diffusion!(model, 1, κ₂)
-dc²_dκ_fd = (c²₂ - c²₁) / (κ₂ - κ₁)
-
 # Now for real
 amplitude = 1.0
 κ = 1.0
 dmodel = Enzyme.make_zero(model)
+set_diffusivity!(model, κ)
 set_diffusivity!(dmodel, 0)
 
-#autodiff(Reverse, set_initial_condition!, Duplicated(model, dmodel), Active(amplitude))
-#autodiff(Reverse, set_diffusivity!, Duplicated(model, dmodel), Active(κ))
-
 dc²_dκ = autodiff(Enzyme.Reverse,
-                  stable_diffusion!,
+                  set_initial_condition!,
                   Duplicated(model, dmodel),
-                  Const(amplitude),
-                  Active(κ))
+                  Const(amplitude))
 
 @info """ \n
 Enzyme computed $dc²_dκ
-Finite differences computed $dc²_dκ_fd
 """
