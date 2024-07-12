8bf4717e1c32737aa7bc9c1f08aa97f8e381b917
jlk9 <josek97@utexas.edu>
2024-07-12 09:16:54 -0500

    Reduced for Flux BC error

--- stable_diffusion/stable_diffusion.jl
+++ stable_diffusion/stable_diffusion.jl
@@ -1,109 +1,38 @@
-# Currently, running this requires
-# - Enzyme#main (eg, > v0.11.11)
-
-using Oceananigans
-using Oceananigans.TurbulenceClosures: with_tracers
-using Oceananigans.BoundaryConditions: fill_halo_regions!
-using Oceananigans.Fields: ConstantField
-using Oceananigans.Models.HydrostaticFreeSurfaceModels: tracernames
+using Oceananigans.BoundaryConditions: PeriodicBoundaryCondition, FluxBoundaryCondition
 using Enzyme
 
 Enzyme.API.runtimeActivity!(true)
-#Enzyme.API.printall!(true)
-#Enzyme.API.printactivity!(true)
 Enzyme.API.looseTypeAnalysis!(true)
-#Enzyme.EnzymeRules.inactive_type(::Type{<:Oceananigans.Grids.AbstractGrid}) = true
-Enzyme.EnzymeRules.inactive_type(::Type{<:Oceananigans.Clock}) = true
 Enzyme.EnzymeRules.inactive_noinl(::typeof(Core._compute_sparams), args...) = nothing
 
-const maximum_diffusivity = 100
-
-Nx = Ny = 64
-Nz = 8
-
-x = y = (-π, π)
-z = (-0.5, 0.5)
-topology = (Periodic, Periodic, Bounded)
-
-grid = RectilinearGrid(size=(Nx, Ny, Nz); x, y, z, topology)
-diffusion = VerticalScalarDiffusivity(κ=0.1)
-
-u = XFaceField(grid)
-v = YFaceField(grid)
-
-# ψ(x, y) = cos(x) * cos(y)
-# u = - ∂y ψ = - cos(x) * sin(y)
-# v = + ∂x ψ = + sin(x) * cos(y)
-# ... and scale with U:
-U = 1
-u₀(x, y, z) = - U * cos(x + π/4) * sin(y) * (z + 0.5)
-v₀(x, y, z) = + U * sin(x + π/4) * cos(y) * (z + 0.5)
-
-set!(u, u₀)
-set!(v, v₀)
-fill_halo_regions!(u)
-fill_halo_regions!(v)
-
 @inline function tracer_flux(x, y, t, c, p)
-    c₀ = p.surface_tracer_concentration
-    u★ = p.piston_velocity
-    return - u★ * (c₀ - c)
+    return c
 end
 
-parameters = (surface_tracer_concentration = 1,
-              piston_velocity = 0.1)
+parameters = (a = 1, b = 0.1)
 
-top_c_bc = FluxBoundaryCondition(tracer_flux, field_dependencies=:c; parameters)
-c_bcs = FieldBoundaryConditions(top=top_c_bc)
 
-# TODO:
-# 1. Make the velocity fields evolve
-# 2. Add surface fluxes
-# 3. Do a problem where we invert for the tracer fluxes (maybe with CATKE)
+@inline extract_bc(bc, ::Val{:north}) = (bc.north)
+@inline extract_bc(bc, ::Val{:top}) = (bc.top)
 
-model = HydrostaticFreeSurfaceModel(; grid,
-                                    tracer_advection = WENO(),
-                                    tracers = :c,
-                                    buoyancy = nothing,
-                                    velocities = PrescribedVelocityFields(; u, v),
-                                    boundary_conditions = (; c=c_bcs),
-                                    closure = diffusion)
-
-"""
-    set_diffusivity!(model, diffusivity)
-
-Change diffusivity of model to `diffusivity`.
-"""
-function set_diffusivity!(model, diffusivity)
-    closure = VerticalScalarDiffusivity(; κ=diffusivity)
-    names = tuple(:c) # tracernames(model.tracers)
-    closure = with_tracers(names, closure)
-    model.closure = closure
-    return nothing
-end
-
-function set_initial_condition!(model, amplitude)
-    # Set initial condition
-    amplitude = Ref(amplitude)
-
-    # This has a "width" of 0.1
-    cᵢ(x, y, z) = amplitude[]
-    set!(model, c=cᵢ)
+function permute_boundary_conditions(boundary_conditions)
+    sides = [:north, :top]
+    sides = sides[[2, 1]]
+    boundary_conditions = Tuple(extract_bc(boundary_conditions, Val(side)) for side in sides)
 
     return nothing
 end
 
-# Now for real
-amplitude = 1.0
-κ = 1.0
-dmodel = Enzyme.make_zero(model)
-set_diffusivity!(model, κ)
-set_diffusivity!(dmodel, 0)
+bc = (north=PeriodicBoundaryCondition(), top=FluxBoundaryCondition(tracer_flux, field_dependencies=:c; parameters))
+
+d_bc = Enzyme.make_zero(bc)
+
+@show bc
+@show d_bc
 
 dc²_dκ = autodiff(Enzyme.Reverse,
-                  set_initial_condition!,
-                  Duplicated(model, dmodel),
-                  Const(amplitude))
+                  permute_boundary_conditions,
+                  Duplicated(bc, d_bc))
 
 @info """ \n
 Enzyme computed $dc²_dκ
