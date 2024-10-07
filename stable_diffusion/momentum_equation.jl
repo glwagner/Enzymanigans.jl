ea69643148964b76a716bc0c92fe0ecc8bdc9fb2
jlk9 <josek97@utexas.edu>
2024-10-07 09:13:33 -0500

    Split explicit

--- stable_diffusion/momentum_equation.jl
+++ stable_diffusion/momentum_equation.jl
@@ -84,7 +84,7 @@ buoyancy = SeawaterBuoyancy(equation_of_state=LinearEquationOfState())
 
 model = HydrostaticFreeSurfaceModel(; grid,
                                     momentum_advection = WENO(),
-                                    free_surface = ExplicitFreeSurface(gravitational_acceleration=1),
+                                    free_surface = SplitExplicitFreeSurface(grid, cfl=0.7, gravitational_acceleration=1),
                                     #buoyancy = buoyancy,
                                     #tracers = (:T, :S),
                                     #velocities = PrescribedVelocityFields(; u, v),
